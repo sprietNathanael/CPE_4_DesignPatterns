216b3fcc0962f3df100917c8d7cad606402a1df8
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-04-27 13:34:15 +0200

    Prepares the project files

---
+++ Exercices/JeuEchec/src/launcher/localLauncher/LauncherCmdLine.java
@@ -0,0 +1,32 @@
+package launcher.localLauncher;
+
+import java.util.Observer;
+
+import vue.ChessGameCmdLine;
+import controler.ChessGameControlers;
+
+
+
+/**
+ * @author francoise.perrin
+ * Lance l'exécution d'un jeu d'échec en mode console.
+ */
+public class LauncherCmdLine {
+	
+	public static void main(String[] args) {		
+		
+		ChessGame model;
+		ChessGameControlers controler;	
+		ChessGameCmdLine vue;
+		
+		model = new ChessGame();	
+		controler = new ChessGameControler(model);
+		
+		new ChessGameCmdLine(controler);	
+		
+		vue = new ChessGameCmdLine(controler);
+	
+		vue.go();
+	}
+
+}
